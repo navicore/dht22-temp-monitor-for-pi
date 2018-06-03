2ffbbb77964fd2041a40a8c6e6daa655a0bccb13
Ed Sweeney <ed@onextent.com>
2018-06-02 18:03:08 -0700

    float marshalling

--- src/main/scala/onextent/iot/dht22/Dht22.scala
+++ src/main/scala/onextent/iot/dht22/Dht22.scala
@@ -4,10 +4,9 @@ import ch.jodersky.jni.nativeLoader
 
 @nativeLoader("dht220")
 class Dht22 {
+
   // --- Native methods
-  @native def intMethod(n: Int): Int
-  @native def booleanMethod(b: Boolean): Boolean
-  @native def stringMethod(s: String): String
-  @native def intArrayMethod(a: Array[Int]): Int
+  @native def get(): Array[Float]
+
 }
 
--- src/main/scala/onextent/iot/dht22/Main.scala
+++ src/main/scala/onextent/iot/dht22/Main.scala
@@ -2,18 +2,17 @@ package onextent.iot.dht22
 
 object Main {
 
-  // --- Main method to test our native library
   def main(args: Array[String]): Unit = {
-    val dht22 = new Dht22
-    val square = dht22.intMethod(5)
-    val bool = dht22.booleanMethod(true)
-    val text = dht22.stringMethod("java")
-    val sum = dht22.intArrayMethod(Array(1, 1, 2, 3, 5, 8, 13))
 
-    println(s"intMethod: $square")
-    println(s"booleanMethod: $bool")
-    println(s"stringMethod: $text")
-    println(s"intArrayMethod: $sum")
+    val dht22 = new Dht22
+
+    val reading: Array[Float] = dht22.get()
+
+    println(s"reading c: ${reading(0)}")
+    println(s"reading f: ${reading(1)}")
+    println(s"reading h: ${reading(2)}")
+
   }
+
 }
 
